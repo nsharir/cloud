fd86befb39502807abcc781bd8c3e672f271e346
nsharir <n.sharir@gmail.com>
2015-08-11 22:19:09 +0300

    D&D seed

--- web-server/src/main/resources/web-static-files/js/main.js
+++ web-server/src/main/resources/web-static-files/js/main.js
@@ -1,4 +1,18 @@
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev,data) {
+    ev.dataTransfer.setData("draggedObject", angular.toJson(data));
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = angular.fromJson(ev.dataTransfer.getData("draggedObject"));
+
+}
+
 angular.module('app');
 
 function cloudMainCtrl($scope , $http ) {
